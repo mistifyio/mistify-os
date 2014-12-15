f2c1fbe0a452a9741b7d9e3448e5de4c0f6436a0
Dale Ghent <daleg@omniti.com>
2014-12-15 23:57:37 +0000

    systemd: conversion checkpoint

--- package/mistify/libvirt/libvirt.mk
+++ package/mistify/libvirt/libvirt.mk
@@ -12,26 +12,23 @@ LIBVIRT_LICENSE_FILES = COPYING COPYING.LESSER
 LIBVIRT_DEPENDENCIES  = host-pkgconf lvm2 libnl libxml2 yajl libpciaccess
 HOST_LIBVIRT_DEPENDENCIES = host-pkgconf host-libxml2 host-hlibnl host-hyajl host-lvm2
 
-define LIBVIRT_INSTALL_INIT_SYSV
-	$(INSTALL) -m 755 -D \
-		$(BR2_EXTERNAL)/package/mistify/libvirt/libvirt-bin.init \
-	$(TARGET_DIR)/etc/init.d/S65libvirt-bin
+define LIBVIRT_INSTALL_SYSCONFIG
+	mkdir -p $(TARGET_DIR)/etc/sysconfig
+	$(INSTALL) -D -m 644 \
+		$(BR2_EXTERNAL)/package/mistify/libvirt/libvirtd.sysconfig \
+		$(TARGET_DIR)/etc/sysconfig/libvirtd
 endef
 
-define LIBVIRT_INSTALL_DEFAULTS
-	$(INSTALL) -m 644 -D \
-		$(BR2_EXTERNAL)/package/mistify/libvirt/libvirt-bin.defaults \
-		$(TARGET_DIR)/etc/default/libvirt-bin
+define LIBVIRT_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 \
+		$(BR2_EXTERNAL)/package/mistify/libvirt/libvirtd.service \
+		$(TARGET_DIR)/etc/systemd/system/libvirtd.service
+	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+	ln -fs ../libvirtd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/libvirtd.service
 endef
 
-define LIBVIRT_INSTALL_STOPSCRIPT
-	test -s $(TARGET_DIR)/etc/init.d/K40libvirt-bin || \
-		(cd $(TARGET_DIR)/etc/init.d && ln -s ./S65libvirt-bin \
-			K40libvirt-bin)
-endef
-
-LIBVIRT_POST_INSTALL_TARGET_HOOKS += LIBVIRT_INSTALL_DEFAULTS
-LIBVIRT_POST_INSTALL_TARGET_HOOKS += LIBVIRT_INSTALL_STOPSCRIPT
+LIBVIRT_POST_INSTALL_TARGET_HOOKS += LIBVIRT_INSTALL_SYSCONFIG
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
