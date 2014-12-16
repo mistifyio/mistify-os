fe31f3954e2a0d033091b8b3b49e00790b7699f5
Dale Ghent <daleg@omniti.com>
2014-12-16 06:18:39 +0000

    libvirt: start libvirtd under systemd

--- package/mistify/libvirt/libvirt.mk
+++ package/mistify/libvirt/libvirt.mk
@@ -9,8 +9,17 @@ LIBVIRT_SOURCE        = libvirt-$(LIBVIRT_VERSION).tar.gz
 LIBVIRT_SITE          = http://libvirt.org/sources/
 LIBVIRT_LICENSE       = GPLv2 LGPLv2.1
 LIBVIRT_LICENSE_FILES = COPYING COPYING.LESSER
-LIBVIRT_DEPENDENCIES  = host-pkgconf lvm2 libnl libxml2 yajl libpciaccess
-HOST_LIBVIRT_DEPENDENCIES = host-pkgconf host-libxml2 host-hlibnl host-hyajl host-lvm2
+LIBVIRT_DEPENDENCIES  = host-pkgconf lvm2 libnl libxml2 yajl libpciaccess systemd iptables ebtables
+HOST_LIBVIRT_DEPENDENCIES = host-pkgconf host-libxml2 host-hlibnl host-hyajl host-lvm2 systemd iptables ebtables
+
+LIBVIRT_CONF_ENV += IPTABLES_PATH=/usr/sbin/iptables
+LIBVIRT_CONF_ENV += IP6TABLES_PATH=/usr/sbin/ip6tables
+
+HOST_LIBVIRT_CONF_ENV += IPTABLES_PATH=/usr/sbin/iptables
+HOST_LIBVIRT_CONF_ENV += IP6TABLES_PATH=/usr/sbin/ip6tables
+
+LIBVIRT_CONF_OPTS += --with-init-script=systemd
+HOST_LIBVIRT_CONF_OPTS += --with-init-script=systemd
 
 define LIBVIRT_INSTALL_SYSCONFIG
 	mkdir -p $(TARGET_DIR)/etc/sysconfig
@@ -20,12 +29,13 @@ define LIBVIRT_INSTALL_SYSCONFIG
 endef
 
 define LIBVIRT_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 \
-		$(BR2_EXTERNAL)/package/mistify/libvirt/libvirtd.service \
-		$(TARGET_DIR)/etc/systemd/system/libvirtd.service
 	mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
-	ln -fs ../libvirtd.service \
+	ln -fs /usr/lib/systemd/system/libvirtd.service \
 		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/libvirtd.service
+	ln -fs /usr/lib/systemd/system/libvirt-guests.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/libvirt-guests.service
+	ln -fs /usr/lib/systemd/system/virtlockd.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/virtlockd.service
 endef
 
 LIBVIRT_POST_INSTALL_TARGET_HOOKS += LIBVIRT_INSTALL_SYSCONFIG
