57e4ffa09a8860dea80d3dbada5c81b8f9ef3c93
Dale Ghent <daleg@omniti.com>
2014-12-19 23:20:56 +0000

    convert mistify-agent-libvirt to systemd

--- package/mistify/mistify-agent-libvirt/mistify-agent-libvirt.mk
+++ package/mistify/mistify-agent-libvirt/mistify-agent-libvirt.mk
@@ -25,17 +25,21 @@ define MISTIFY_AGENT_LIBVIRT_BUILD_CMDS
 	GOPATH=$(GOPATH) make install DESTDIR=$(TARGET_DIR) \
 	 -C $(GOPATH)/src/github.com/mistifyio/mistify-agent-libvirt
 	mv $(TARGET_DIR)/opt/mistify/sbin/mistify-libvirt  $(TARGET_DIR)/opt/mistify/sbin/mistify-agent-libvirt
-    $(INSTALL) -m 755 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-libvirt/run \
-	    $(TARGET_DIR)/etc/sv/mistify-agent-libvirt/run
-	$(INSTALL) -m 755 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-libvirt/log.run \
-	    $(TARGET_DIR)/etc/sv/mistify-agent-libvirt/log/run
-	mkdir -p $(TARGET_DIR)/etc/service
-    cd $(TARGET_DIR)/etc/service && ln -sf ../sv/mistify-agent-libvirt .
-
 endef
 
 define MISTIFY_AGENT_LIBVIRT_INSTALL_CMDS
 	# The install was done as part of the build.
+endef
+
+define MISTIFY_AGENT_LIBVIRT_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -m 644 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-libvirt/mistify-agent-libvirt.service \
+		$(TARGET_DIR)/etc/systemd/system/mistify-agent-libvirt.service
+
+	$(INSTALL) -m 644 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-libvirt/mistify-agent-libvirt.sysconfig \
+		$(TARGET_DIR)/etc/sysconfig/mistify-agent-libvirt
+
+	ln -sf ../mistify-agent-libvirt.service \
+		$(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/mistify-agent-libvirt.service
 endef
 
 $(eval $(generic-package))
