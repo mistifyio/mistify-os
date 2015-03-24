7144f756f94ef3277d2baa12f23af7d8dd2f7d5a
Steve Isaacs <sisaacs@omniti.com>
2015-03-24 17:06:01 -0400

    Added for shared defaults and a couple of functions for simplifying settting and saving default values.

--- test/scripts/mistify-test-defaults.sh
+++ test/scripts/mistify-test-defaults.sh
@@ -2,20 +2,44 @@
 # Defaults for testing Mistify-OS.
 #-
 
-# Network configuration
-tapdefault=tap0
-bridgedefault=mosbr0
-ipdefault=10.0.2.2
-maskbitsdefault=24
-macdefault=DE:AD:BE:EF:`printf "%02X:%02X" $(( (RANDOM % 256) )) $(( (RANDOM % 256) ))`
+testmistifystatedir=${statedir}/testmistify
 
-# Disk images
-diskimagesizedefault=1G
+function get_default() {
+    # Parameters:
+    #   1: option name
+    #   2: default value
+    if [ -e $testmistifystatedir/$1 ]; then
+      r=`cat $testmistifystatedir/$1`
+    else
+      r=$2
+    fi
+    verbose The default for $1 is $2
+    echo $r
+}
+
+function set_default() {
+    # Parameters:
+    #   1: option name
+    #   2: value
+    echo "$2">$testmistifystatedir/$1
+    verbose The default $1 has been set to $2
+}
+
+# Network configuration
+
+tapdefault=$(get_default tap tap0)
+bridgedefault=$(get_default bridge mosbr0)
+bridgeipdefault=$(get_default bridgeip 10.0.2.2)
+maskbitsdefault=$(get_default maskbits 24)
 
 # Build information.
 if [ -e "$statedir/variantbuilddir" ]; then
-  builddirdefault=`cat $statedir/variantbuilddir`
+    builddirdefault=$(get_default builddir `cat $statedir/variantbuilddir`)
+else
+    builddirdefault=$(get_default builddir '$PWD')
 fi
 
-testmistifystatedir=${statedir}/testmistify
+# Test images
+kerneldefault=$(get_default kernel $builddirdefault/images/bzImage.buildroot)
+initrddefault=$(get_default initrd $builddirdefault/images/initrd.buildroot)
 
