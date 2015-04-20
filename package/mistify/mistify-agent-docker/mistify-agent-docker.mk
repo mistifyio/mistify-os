2aa5bc152b31431055a10865f51caecb1bb12e73
Manuel Mendez <mmendez534@gmail.com>
2015-04-20 16:54:56 -0400

    packages: add mistify-agent-docker

---
+++ package/mistify/mistify-agent-docker/mistify-agent-docker.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# mistify-agent-docker
+#
+################################################################################
+
+MISTIFY_AGENT_DOCKER_VERSION = 2e944c12bc03709c9a4f4c30d3b6df0b97c0b046
+MISTIFY_AGENT_DOCKER_SITE    = git@github.com:mistifyio/mistify-agent-docker.git
+MISTIFY_AGENT_DOCKER_SITE_METHOD = git
+MISTIFY_AGENT_DOCKER_LICENSE = Apache
+MISTIFY_AGENT_DOCKER_LICENSE_FILES = LICENSE
+MISTIFY_AGENT_DOCKER_DEPENDENCIES = docker-docker mistify-agent
+
+GOPATH=$(O)/tmp/GOPATH
+
+define MISTIFY_AGENT_DOCKER_BUILD_CMDS
+	# GO apparently wants the install path to be independent of the
+	# build path. Use a temporary directory to do the build.
+	mkdir -p $(GOPATH)/src/github.com/mistifyio/mistify-agent-docker
+	rsync -av --delete-after --exclude=.git --exclude-from=$(@D)/.gitignore \
+		$(@D)/ $(GOPATH)/src/github.com/mistifyio/mistify-agent-docker/
+	GOROOT=$(GOROOT) \
+	PATH=$(GOROOT)/bin:$(PATH) \
+	GOPATH=$(GOPATH) make install DESTDIR=$(TARGET_DIR) \
+		-C $(GOPATH)/src/github.com/mistifyio/mistify-agent-docker
+endef
+
+define MISTIFY_AGENT_DOCKER_INSTALL_TARGET_CMDS
+	# The install was done as part of the build.
+endef
+
+define MISTIFY_AGENT_DOCKER_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -m 644 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-docker/mistify-agent-docker.service \
+		$(TARGET_DIR)/lib/systemd/system/mistify-agent-docker.service
+
+	$(INSTALL) -m 644 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-docker/mistify-agent-docker.sysconfig \
+		$(TARGET_DIR)/etc/sysconfig/mistify-agent-docker
+endef
+
+$(eval $(generic-package))
