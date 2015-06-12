86caa3d1d83f4d5ad04a618e7ef3fa44621f539c
Steve Isaacs <sisaacs@omniti.com>
2015-06-11 20:11:08 -0400

    Clarified some messages to indicate test related.

--- test/scripts/mistify-test-defaults.sh
+++ test/scripts/mistify-test-defaults.sh
@@ -25,7 +25,7 @@ function get_test_default() {
     else
       r=$2
     fi
-    verbose The default for $1 is $2
+    verbose The test default for $1 is $2
     echo $r
 }
 
@@ -34,7 +34,7 @@ function set_test_default() {
     #   1: option name
     #   2: value
     echo "$2">$testmistifystatedir/$1
-    verbose The default $1 has been set to $2
+    verbose The test default $1 has been set to $2
 }
 
 function reset_test_default() {
@@ -42,9 +42,9 @@ function reset_test_default() {
     #   1: option name
     if [ -e $testmistifystatedir/$1 ]; then
       rm $testmistifystatedir/$1
-      verbose Option $1 default has been reset.
+      verbose Option $1 test default has been reset.
     else
-      verbose Option $1 has not been set.
+      verbose Option $1 test default has not been set.
     fi
 }
 
