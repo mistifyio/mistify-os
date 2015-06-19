2139fe274fc1df25dc07b1304fda16c864cc7537
Steve Isaacs <sisaacs@omniti.com>
2015-06-19 14:03:39 -0400

    Worked around a compile and link problem related to lzma by adding dependencies on host-xz (lzma.h) and host-lzma (liblzma.so) which are installed in the target's usr/include and usr/lib directories. To complete the workaround used the CPATH and LIBRARY_PATH environment variables to pass the correct target directory paths to gcc. The IPXE make file doesn't modify these variables.

--- package/mistify/ipxe/ipxe.mk
+++ package/mistify/ipxe/ipxe.mk
@@ -6,10 +6,13 @@
 
 IPXE_VERSION       = 6b7157c233541a4cb3c90021e8ca219b0b5dd358
 IPXE_SITE          = git://git.ipxe.org/ipxe.git
+IPXE_DEPENDENCIES  = host-xz host-lzma
 IPXE_LICENSE       = GPLv2
 IPXE_LICENSE_FILES = COPYING COPYING.GPLv2
 
 define IPXE_BUILD_CMDS
+	export CPATH=$(TARGET_DIR)/usr/include && \
+	export LIBRARY_PATH=$(TARGET_DIR)/usr/lib && \
 	cp $(BR2_EXTERNAL)/package/mistify/ipxe/netboot.ipxe \
 		$(IPXE_DIR)/src && \
 	cd $(IPXE_DIR)/src && \
