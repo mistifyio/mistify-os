18d58eb264f38b0d295aabce5e4fd59b7ada1ce2
Manuel Mendez <mmendez534@gmail.com>
2015-07-07 12:18:49 -0400

    update packages to latest masters

--- package/mistify/mistify-image-service/mistify-image-service.mk
+++ package/mistify/mistify-image-service/mistify-image-service.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MISTIFY_IMAGE_SERVICE_VERSION = c8d3729552b4cf6b6699d3bf4b2553bef8e5710c
+MISTIFY_IMAGE_SERVICE_VERSION = 29cb1b98c7683f7fb74d5c1d4518fa5389f35940
 MISTIFY_IMAGE_SERVICE_SITE    = git@github.com:mistifyio/mistify-image-service.git
 MISTIFY_IMAGE_SERVICE_SITE_METHOD = git
 MISTIFY_IMAGE_SERVICE_LICENSE = Apache
