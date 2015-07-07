b43673f9480f4c70d3607afc7580b647b3ccd749
Manuel Mendez <mmendez534@gmail.com>
2015-07-07 12:18:49 -0400

    update packages to latest masters

--- package/mistify/mistify-agent-image/mistify-agent-image.mk
+++ package/mistify/mistify-agent-image/mistify-agent-image.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MISTIFY_AGENT_IMAGE_VERSION = f9af303cc77dc9f53d8846bbc1d6f9a4f1485e18
+MISTIFY_AGENT_IMAGE_VERSION = 81881ad6f3f7666f83c8f7ad95b434215485ae2a
 MISTIFY_AGENT_IMAGE_SITE    = git@github.com:mistifyio/mistify-agent-image.git
 MISTIFY_AGENT_IMAGE_SITE_METHOD = git
 MISTIFY_AGENT_IMAGE_LICENSE = Apache
