06f843572c84f1e6f1455b248db79576adbc051b
Brian Bickerton <bbickerton@omniti.com>
2015-07-10 10:57:23 -0400

    Add mistify ovs subagent package (mistify-agent-ovs)

---
+++ package/mistify/mistify-agent-ovs/mistify-agent-ovs.mk
@@ -0,0 +1,40 @@
+################################################################################
+#
+# mistify-agent-ovs
+#
+################################################################################
+
+MISTIFY_AGENT_OVS_VERSION = 018c8b2165d40e75b7d93083eb26983a557e62a9
+MISTIFY_AGENT_OVS_SITE    = git@github.com:mistifyio/mistify-agent-ovs.git
+MISTIFY_AGENT_OVS_SITE_METHOD = git
+MISTIFY_AGENT_OVS_LICENSE = Apache
+MISTIFY_AGENT_OVS_LICENSE_FILES = LICENSE
+MISTIFY_AGENT_OVS_DEPENDENCIES = mistify-agent
+
+GOPATH=$(O)/tmp/GOPATH
+
+define MISTIFY_AGENT_OVS_BUILD_CMDS
+	# GO apparently wants the install path to be independent of the
+	# build path. Use a temporary directory to do the build.
+	mkdir -p $(GOPATH)/src/github.com/mistifyio/mistify-agent-ovs
+	rsync -av --delete-after --exclude=.git --exclude-from=$(@D)/.gitignore \
+		$(@D)/ $(GOPATH)/src/github.com/mistifyio/mistify-agent-ovs/
+	GOROOT=$(GOROOT) \
+	PATH=$(GOROOT)/bin:$(PATH) \
+	GOPATH=$(GOPATH) make install DESTDIR=$(TARGET_DIR) \
+		-C $(GOPATH)/src/github.com/mistifyio/mistify-agent-ovs
+endef
+
+define MISTIFY_AGENT_OVS_INSTALL_TARGET_CMDS
+	# The install was done as part of the build.
+endef
+
+define MISTIFY_AGENT_OVS_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -m 644 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-ovs/mistify-agent-ovs.service \
+		$(TARGET_DIR)/lib/systemd/system/mistify-agent-ovs.service
+
+	$(INSTALL) -m 644 -D $(BR2_EXTERNAL)/package/mistify/mistify-agent-ovs/mistify-agent-ovs.sysconfig \
+		$(TARGET_DIR)/etc/sysconfig/mistify-agent-ovs
+endef
+
+$(eval $(generic-package))
