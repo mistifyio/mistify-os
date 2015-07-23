fc9001c14303862d371685e7745988d36347110b
Dale Ghent <daleg@omniti.com>
2015-07-23 16:25:37 +0000

    variant: add build support for dev variant

--- external.mk
+++ external.mk
@@ -1,1 +1,10 @@
 include $(sort $(wildcard $(BR2_EXTERNAL)/package/*/*/*.mk))
+
+define VARIANT_DEV_COPY_HEADERS
+	(cd $(STAGING_DIR)/usr && \
+		tar cf - include | (cd $(TARGET_DIR)/usr && tar xf -))
+endef
+
+ifeq ($(BR2_VARIANT_DEV),y)
+	TARGET_FINALIZE_HOOKS += VARIANT_DEV_COPY_HEADERS
+endif
