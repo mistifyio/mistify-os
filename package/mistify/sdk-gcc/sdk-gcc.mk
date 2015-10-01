60241683fdc25b305d07d9c299abef15ed3c9bf3
Steve Isaacs <sisaacs@omniti.com>
2015-10-01 13:49:38 -0400

    Buildroot removes files when packaing the target fs this causes some of the key files needed to actually use gcc to dissappear. When installing to both the target and staging directories this causes a relink of some libraries which fails the second time around. To avoid this installing only to the target directory and then copying to the staging directory. This was done to work around this problem while focusing on getting gcc running on mistify-os and will be dealt properly later.

--- package/mistify/sdk-gcc/sdk-gcc.mk
+++ package/mistify/sdk-gcc/sdk-gcc.mk
@@ -9,6 +9,7 @@ SDK_GCC_SITE = http://www.netgull.com/gcc/releases/gcc-$(SDK_GCC_VERSION)
 SDK_GCC_SOURCE = gcc-$(SDK_GCC_VERSION).tar.bz2
 SDK_GCC_LICENSE = GPLv3+
 SDK_GCC_LICENSE_FILES = COPYING
+# SDK_GCC_INSTALL_STAGING = YES
 
 define SDK_GCC_BUILD_CMDS
         cd $(@D) && \
@@ -16,14 +17,38 @@ define SDK_GCC_BUILD_CMDS
 	mkdir ../sdk-gcc-build && \
 	cd ../sdk-gcc-build && \
 	$(@D)/configure \
-	--prefix=$(TARGET_DIR)/usr \
+	--prefix=/usr \
 	--enable-shared \
 	--enable-threads=posix \
 	--enable-__cxa_atexit \
 	--enable-clocale=gnu \
 	--enable-languages=c,c++,go,objc,obj-c++,fortran \
+	--with-gxx-include-dir=/usr/include/c++/$(SDK_GCC_VERSION) \
 	--disable-multilib  && \
 	make CC="$(TARGET_CC)" LD="$(TARGET_LD)"
 endef
+
+
+define SDK_GCC_INSTALL_STAGING_CMDS
+	cd $(@D)/../sdk-gcc-build && \
+	make DESTDIR=$(STAGING_DIR) install
+endef
+
+
+
+define SDK_GCC_INSTALL_TARGET_CMDS
+	cd $(@D)/../sdk-gcc-build && \
+	make DESTDIR=$(TARGET_DIR) install
+	#  HACK ALERT
+	# Buildroot removes the include files from the target directory before
+	# creating the target file system and the gcc build for some reason
+	# produces a link error when attempting to install to both the staging
+	# and target directories. So, to work around install to the target and
+	# then copy the header files to staging. These are then copied back
+	# out of staging in the post build script. This way the link problem
+	# can be solved later.
+	cp -r $(TARGET_DIR)/usr/include/c++ $(STAGING_DIR)/usr/include
+endef
+
 
 $(eval $(generic-package))
