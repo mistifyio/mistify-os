3916098c946f9a8430410c9a2beea6b0051458fb
Steve Isaacs <sisaacs@omniti.com>
2016-04-19 13:29:38 -0400

    Switched back to 4.9.3 because other container related problems were breaking the toolchain build and these are also breaking the build itself.

--- package/mistify/sdk-gcc/sdk-gcc.mk
+++ package/mistify/sdk-gcc/sdk-gcc.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-SDK_GCC_VERSION = 5.3.0
+SDK_GCC_VERSION = 4.9.3
 SDK_GCC_SITE = $(BR2_GNU_MIRROR)/gcc/releases/gcc-$(SDK_GCC_VERSION)
 SDK_GCC_SOURCE = gcc-$(SDK_GCC_VERSION).tar.bz2
 SDK_GCC_LICENSE = GPLv3+
